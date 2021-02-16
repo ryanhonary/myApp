c4e113b03533b630ffe432edc28ae0141bf893fb
Ryan Honary <ryanhonary@ryans-mbp.attlocal.net>
2021-02-16 09:16:13 -0800

    we added the potato counter and some snack bars to header container

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,14 +1,16 @@
-import { AppBar, Toolbar, IconButton, Typography, Button } from "@material-ui/core";
+import { AppBar, Toolbar, IconButton, Typography, Button, Snackbar } from "@material-ui/core";
 import { Apple } from "@material-ui/icons";
 import React from "react";
 
 export interface HeaderProps {
     count: number,
     incrementClickCount: () => void,
+    potatoCounter: () => void,
+    potatoPicker: number
 }
 
 const Header = (props: HeaderProps) => {
-    const { count, incrementClickCount } = props; // props.count or props.incrementClickCount()
+    const { count, incrementClickCount, potatoCounter, potatoPicker } = props; // props.count or props.incrementClickCount()
     return (
         <AppBar position="static">
             <Toolbar>
@@ -18,22 +20,21 @@ const Header = (props: HeaderProps) => {
                 <Typography variant="h6" >
                     News
                 </Typography>
-                <Button color="inherit">Login</Button>
-                <Button color="inherit">Smack!</Button>
-                <Button color="inherit"><Apple /></Button>
-                <Button>default</Button>
-                <Button color="secondary">I AM PINK</Button>
-                <Button variant="outlined">I AM OUTLINED</Button>
-                <Button disabled>I AM DISABLED</Button>
-                <Button
-                    color="secondary"
-                    variant="outlined"
-                    size="large"
-                    disabled={count > 10}
-                    onClick={() => incrementClickCount()}
-                >
+                {/* <Button color="inherit">Login</Button> */}
+                {/* <Button color="inherit">Smack!</Button> */}
+                {/* <Button color="inherit"><Apple /></Button> */}
+                {/* <Button>default</Button> */}
+                {/* <Button color="secondary">I AM PINK</Button> */}
+                {/* <Button variant="outlined">I AM OUTLINED</Button> */}
+                {/* <Button disabled>I AM DISABLED</Button> */}
+                <Button disabled={count > 10} onClick={() => incrementClickCount()}>
                     NEW BUTTON: {count}
                 </Button>
+                <Button onClick={() => potatoCounter()}>
+                    Potatos clicked: {potatoPicker}
+                </Button>
+                <Snackbar open={potatoPicker===12} message='You have achieved an upgrade!'></Snackbar>
+                <Snackbar open={potatoPicker===20 && count===10} message='YOU WIN!'></Snackbar>
             </Toolbar>
         </AppBar>
     )
@@ -42,3 +43,13 @@ const Header = (props: HeaderProps) => {
 
 
 export default Header;
+
+
+
+
+
+
+//1. Make a potato counter function
+	//2. useState to make a potatoCount variable
+	//3. Every click is +2 potatoes
+//When 12 potatoes are ready, create a Material UI <Snackbar> that pops up saying "12 Potatoes Read"
--- src/HeaderContainer.tsx
+++ src/HeaderContainer.tsx
@@ -1,19 +1,34 @@
-import react, {useState} from 'react';
+import {useState} from 'react';
 import Header from './Header';
 
 const HeaderContainer = () => {
     const [count, setCount] = useState(0);
+    const [potatoPicker, setPotatos] = useState(0);
 
     const incrementClickCount = () => {
         setCount(count + 1);
     }
 
+    const potatoCounter = () => {
+       setPotatos(potatoPicker + 2);
+    }
+    
+
     return (
         <Header
             count={count}
             incrementClickCount={incrementClickCount}
+            potatoPicker={potatoPicker}
+            potatoCounter={potatoCounter}
         />
     )
 }
 
 export default HeaderContainer;
+
+
+
+//1. Make a potato counter function
+	//2. useState to make a potatoCount variable
+	//3. Every click is +2 potatoes
+//When 12 potatoes are ready, create a Material UI <Snackbar> that pops up saying "12 Potatoes Read"
